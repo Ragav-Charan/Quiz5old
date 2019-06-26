0825e270ff4e664e767d24acf56d6e7f8f166f38
Ragav-Charan <charan.ragav@gmail.com>
2019-06-26 18:31:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 port = int(os.getenv('VCAP_APP_PORT','5000'))
-server = 'charan.database.windows.net'
-database = 'MyDB'
-username = 'charan123'
-password = 'Smokescreen@5'
-driver= '{ODBC Driver 17 for SQL Server}'
+
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 @app.route('/')
